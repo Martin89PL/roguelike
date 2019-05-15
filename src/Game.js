90f715f8af7bb15200b771d8f4613c3db6a86281
Marcin <marcingladkowski@gmail.com>
2019-05-15 22:21:10 +0200

    (feat) Implement Game class

--- src/Game.js
+++ src/Game.js
@@ -1,3 +1,6 @@
+import Hero from "./Hero";
+import { tsMethodSignature } from "@babel/types";
+
 const floor = '.';
 
 const BOARD_WIDTH = 28;
@@ -7,7 +10,11 @@ export default class Game {
 
   constructor() {
     this.board = [];
+    this.createBoard(); 
+    this.setHero(new Hero());
+  }
 
+  createBoard() {
     for (let y = 0; y < BOARD_HEIGHT; y++) {
       const row = new Array(BOARD_WIDTH);
       for (let x = 0; x < BOARD_WIDTH; x++) {
@@ -15,6 +22,32 @@ export default class Game {
       }
       this.board[y] = row;
     }
+  }
+
+  move(direction) {
+    let heroCoords = this.hero.getCoords();
+    this.board[heroCoords.getY()][heroCoords.getX()] = '.';
+  
+    switch(direction) {
+      case 'down':
+        this.hero.moveDown();
+        break;
+      case 'right':
+        this.hero.moveRight();
+        break;
+    }
+    heroCoords = this.hero.getCoords();
+    this.board[heroCoords.getY()][heroCoords.getX()] = this.hero.getSign();
+  }
+
+  setHero(hero) {
+    this.hero = hero;
+    const heroCoords = this.hero.getCoords();
+    this.board[heroCoords.getY()][heroCoords.getX()] = this.hero.getSign();
+  }
+
+  getHero() {
+    return this.hero;
   }
 
   draw() {
