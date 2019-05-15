ec1aba4b849e4bff2e36892db8c6f24ed1c94b6e
Marcin <marcingladkowski@gmail.com>
2019-05-15 22:20:51 +0200

    (tests) add Game tests

--- tests/Game.spec.js
+++ tests/Game.spec.js
@@ -6,9 +6,11 @@ test('Game constructor should fill the whole board with floor tiles', () => {
   const game = new Game();
   const board = game.board;
 
+  const allowSigns = ['.', '@'];
+
   for (let y = 0; y < board.length; y++) {
     for (let x = 0; x < board[y].length; x++) {
-      expect(board[y][x]).toBe('.');
+      expect(allowSigns.includes(board[y][x])).toBeTruthy();
     }
   }
 });
@@ -17,4 +19,27 @@ test('Game.draw() should return string', () => {
   const game = new Game();
   const output = game.draw();
   expect(typeof output).toBe('string');
+});
+
+test('Game should contain the hero', () => {
+  const game = new Game();
+  const output = game.draw();
+  expect(output).toContain('@');
+});
+
+test('Game board should let create hero', () => {
+  const game = new Game();
+  expect(typeof game.setHero).toBe('function');
+});
+
+test('Game board should move hero', () => {
+  const game = new Game();
+  game.move('down');
+  game.move('right');
+  const hero = game.getHero();
+  const coords = hero.getCoords();
+
+  expect(coords.getX()).toBe(1);
+  expect(coords.getY()).toBe(1);
+
 });
