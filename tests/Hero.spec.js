64347bf2cb9b27e92812a45bf164ececd8961613
Marcin <marcingladkowski@gmail.com>
2019-05-19 16:49:04 +0200

    (ref) refactor tests for Hero class

--- tests/Hero.spec.js
+++ tests/Hero.spec.js
@@ -1,29 +1,38 @@
 import Hero from '../src/Hero';
 import Coords from '../src/Coords';
 
+describe('testing hero class', () => {
 
-test('Hero should be type of Hero', () => {
-    expect(new Hero()).toBeInstanceOf(Hero);
+    let hero = undefined;
+
+    beforeAll(() => {
+        hero = new Hero();
+    });
+
+    test('Hero should be type of Hero', () => {
+        expect(hero).toBeInstanceOf(Hero);
+    });
+
+    test('Hero should have own sign', () => {
+        expect(hero.getSign()).toBe('@');
+    });
+    
+    test('Hero should have initial coords', () => {
+        let coords = hero.getCoords();
+        expect(coords).toBeInstanceOf(Coords);
+        expect(coords.getX()).toBe(0);
+        expect(coords.getY()).toBe(0);
+    });
+    
+    test('Hero should move down and right once', () => {
+        hero.moveDown();
+        hero.moveRight();
+        let coords = hero.getCoords();
+        expect(coords.getX()).toBe(1);
+        expect(coords.getY()).toBe(1);
+    });
+
 });
 
-test('Hero should have own sign', () => {
-    const hero = new Hero();
-    expect(hero.getSign()).toBe('@');
-});
 
-test('Hero should have initial coords', () => {
-    const hero = new Hero();
-    const coords = hero.getCoords();
-    expect(coords).toBeInstanceOf(Coords);
-    expect(coords.getX()).toBe(0);
-    expect(coords.getY()).toBe(0);
-});
 
-test('Hero should move', () => {
-    const hero = new Hero();
-    hero.moveDown();
-    hero.moveRight();
-    const coords = hero.getCoords();
-    expect(coords.getX()).toBe(1);
-    expect(coords.getY()).toBe(1);
-});
